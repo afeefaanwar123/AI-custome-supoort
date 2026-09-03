c21853195854e1703190ef804529c0495a77cc85
Afifa Anwar <115772987+afeefaanwar123@users.noreply.github.com>
2026-09-03 16:32:07 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 const authService = new AuthService();
 const loginButton = document.querySelector(".login button");
 loginButton.addEventListener("click", handleLogin);
-function handleLogin(){
-        const email = document.getElementById("loginEmail").value;
-    const password = document.getElementById("loginPassword").value;
-    if(email==="" || password===""){
-          console.log("Please fill all fields");
+
+function handleLogin() {
+    const email = document.getElementById("loginEmail").value.trim();
+    const password = document.getElementById("loginPassword").value.trim();
+
+    if (email === "" || password === "") {
+        alert("Please fill all fields");
         return;
-
     }
-    authService.login(email,password);
 
-   
+    authService.login(email, password);
 }
