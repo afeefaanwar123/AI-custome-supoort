6387012fc2d004ee266ef1e8a711cc4e4c2c47e8
Afifa Anwar <115772987+afeefaanwar123@users.noreply.github.com>
2026-09-03 15:59:16 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,33 +4,37 @@ const signupButton = document.querySelector(".signup button");
 
 signupButton.addEventListener("click", handleSignup);
 
-function handleSignup() {
+signup(name, email, password, role) {
 
-    const name = document.getElementById("signupName").value;
-    const email = document.getElementById("signupEmail").value;
-    const password = document.getElementById("signupPassword").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
-    const role = document.getElementById("role").value;
-
-    if (
-        name === "" ||
-        email === "" ||
-        password === "" ||
-        confirmPassword === ""
-    ) {
-        console.log("Please fill all fields");
-        return;
-    }
-
-    if (password !== confirmPassword) {
-        console.log("Passwords do not match");
-        return;
-    }
-
-    authService.signup(
+    const newUser = new User(
         name,
         email,
         password,
         role
     );
+
+    const users =
+        JSON.parse(localStorage.getItem("users")) || [];
+
+    const existingUser = users.find(
+        user => user.email === email
+    );
+
+    if (existingUser) {
+
+        alert("Email already registered");
+
+        return;
+    }
+
+    users.push(newUser);
+
+    localStorage.setItem(
+        "users",
+        JSON.stringify(users)
+    );
+
+    alert("Signup successful!");
+
+    window.location.href = "login.html";
 }
