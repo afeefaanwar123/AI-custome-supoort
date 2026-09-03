1ba047ab7b6566d15e4f02b1e5c9ecedfaa704ff
Afifa Anwar <115772987+afeefaanwar123@users.noreply.github.com>
2026-09-03 16:31:44 +0500

    Update AuthService.js

--- AuthService.js
+++ AuthService.js
@@ -1,74 +1,37 @@
 class AuthService {
+    signup(name, email, password, role) {
+        const newUser = new User(name, email, password, role);
+        const users = JSON.parse(localStorage.getItem("users")) || [];
 
-  signup(name, email, password, role) {
+        const existingUser = users.find(
+            user => user.email.toLowerCase() === email.toLowerCase()
+        );
 
-    const newUser = new User(
-        name,
-        email,
-        password,
-        role
-    );
+        if (existingUser) {
+            return false;
+        }
 
-    console.log(newUser);
-
-    const users =
-        JSON.parse(localStorage.getItem("users")) || [];
-
-    const existingUser =
-        users.find(user => user.email === email);
-
-    if (existingUser) {
-
-        console.log("Email already registered");
-
-        return false;
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+        return true;
     }
 
-    users.push(newUser);
-
-    localStorage.setItem(
-        "users",
-        JSON.stringify(users)
-    );
-
-    console.log("User saved successfully");
-
-    return true;
-}
-
-
     login(email, password) {
-
-        const users =
-            JSON.parse(localStorage.getItem("users")) || [];
-
-        console.log("Users:", users);
-        console.log("Entered email:", email);
-        console.log("Entered password:", password);
+        const users = JSON.parse(localStorage.getItem("users")) || [];
 
         const user = users.find(
             user =>
-                user.email === email &&
+                user.email.toLowerCase() === email.toLowerCase() &&
                 user.password === password
         );
 
-        console.log("Found user:", user);
-
         if (!user) {
-
-            console.log("Invalid email or password");
-
-            return;
+            alert("Invalid email or password");
+            return false;
         }
 
-        console.log("Login successful");
-
-        localStorage.setItem(
-            "currentUser",
-            JSON.stringify(user)
-        );
-
-        window.location.href = "Dashboard.html";
+        localStorage.setItem("currentUser", JSON.stringify(user));
+        window.location.href = "dashboard.html";
+        return true;
     }
-
 }
